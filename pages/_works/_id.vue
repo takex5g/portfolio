aa2021765c203ad404790138a105e5c8716213a4
takex5g <takex5g@gmail.com>
2020-05-09 14:01:28 +0900

    markdownが表示できるようになった

--- pages/_works/_id.vue
+++ pages/_works/_id.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-    <div class="container">ゆうもやだよ ID is {{ $route.params.id }}</div>
-    <div v-html="$md.render('html化したいマークダウンのテキスト')"></div>
+    <div class="container"></div>
+    <div v-html="$md.render(markdown)"></div>
   </div>
 </template>
 
 <script>
+// import Data from "~/store/data.json";
 export default {
-  layout: "WorksSingle"
+  layout: "WorksSingle",
+  asyncData({ params }) {
+    const id = params.id;
+    return {
+      markdown: require("~/assets/workstxt/" + id + ".md")["default"]
+    };
+  }
 };
 </script>
 
