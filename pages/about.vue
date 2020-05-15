e51ccf7e37b31d4a92e9e7c431c5fb43196b36e8
takex5g <takex5g@gmail.com>
2020-05-15 17:18:22 +0900

    とっておきのゆうもやアニメーション

--- pages/about.vue
+++ pages/about.vue
@@ -3,8 +3,16 @@
     <Header page="about" />
     <div class="container">
       <div class="contents">
-        <img src="~/assets/img/takex5g.png" width="100px" height="100px" alt="ゆうもや" />
-        <p>ゆうもや</p>
+        <div class="yumoya">
+          <img
+            :class="{'yumoyaanime': anime}"
+            src="~/assets/img/takex5g_transparent.png"
+            width="100px"
+            height="100px"
+            alt="ゆうもや"
+          />
+        </div>
+        <p class="name">ゆうもや</p>
         <p>ハードウェアからWEBまで。ゆるふわものづくりをやってます</p>
         <div style="padding:10px" />メールはこちら
         <p class="Montserrat">{{takex5g}}🐌monnichi.com</p>
@@ -27,15 +35,59 @@
 import Header from "@/components/Header";
 export default {
   components: { Header },
+  head() {
+    return {
+      title: "About"
+    };
+  },
   data: function() {
     return {
-      takex5g: "takex5g"
+      takex5g: "takex5g",
+      anime: false
     };
+  },
+  methods: {
+    doanime() {
+      this.anime = true;
+      var self = this;
+      setTimeout(function() {
+        self.anime = false;
+      }, (Math.floor(Math.random() * 3) + 1) * 1000);
+    },
+    yumoanime() {
+      var self = this;
+      setTimeout(function() {
+        self.doanime();
+        self.yumoanime();
+      }, (Math.floor(Math.random() * 10) + 5) * 1000);
+    }
+  },
+  mounted() {
+    this.yumoanime();
   }
 };
 </script>
 
 <style scoped>
+.yumoya {
+  width: 100px;
+  height: 100px;
+  background: #b6d55d;
+  border-radius: 50%;
+  margin: auto;
+  overflow: hidden;
+  transition: 1s;
+  position: relative;
+}
+.yumoya img {
+  left: 0px;
+  transition: 1s;
+  top: 0px;
+  position: absolute;
+}
+.yumoyaanime {
+  transform: translate(0px, 40px);
+}
 .container {
   margin-top: 10%;
   margin: auto;
@@ -47,11 +99,19 @@ export default {
   text-align: center;
   margin: 0 auto;
 }
+.name {
+  font-weight: 700;
+}
 .social {
   display: flex;
   justify-content: space-around;
   margin-top: 50px;
+  transition: 0.3s;
 }
+.social:hover {
+  opacity: 0.8;
+}
+
 .social .icon * {
   vertical-align: middle;
 }
