00248c038d4e89eb311df86bf79ce2aaf19c722e
takex5g <takex5g@gmail.com>
2025-12-09 01:01:23 +0900

    WorkCardコンポーネントのスタイルを調整し、リンクの表示を改善

--- components/WorkCard.tsx
+++ components/WorkCard.tsx
@@ -23,7 +23,7 @@ export default function WorkCard({ work, basePath = 'works' }: WorkCardProps) {
             loading="lazy"
             sizes="(max-width: 450px) 160px, 250px"
           />
-          <div className="absolute inset-x-1 bottom-0 px-1 pt-1 bg-white/60 translate-y-[calc(100%-2.5rem)] transition-transform duration-300 group-hover:translate-y-0">
+          <div className="absolute inset-x-0 bottom-0 px-2 pt-1 bg-white/60 translate-y-[calc(100%-2.5rem)] transition-transform duration-300 group-hover:translate-y-0">
             <p className="text-lg font-bold mb-1">{work.title}</p>
             <p className="opacity-0 transition-opacity duration-300 group-hover:opacity-100">
               {work.description}
